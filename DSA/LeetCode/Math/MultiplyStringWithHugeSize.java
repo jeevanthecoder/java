ee3d3f57c0a24d620e9d04c15a3e07c043978e49
Jeevan H <jeevanharish897@gmail.com>
2024-09-02 21:31:22 +0530

    LL

---
+++ DSA/LeetCode/Math/MultiplyStringWithHugeSize.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class MultiplyStringWithHugeSize {
+
+    public static String multiply(String num1, String num2) {
+
+        int m=num1.length(),n=num2.length();
+        int pos[]=new int[m+n];
+        for(int i=m-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                int mul=(num1.charAt(i)-'0')*(num2.charAt(j)-'0');
+                int p1=i+j,p2=i+j+1;
+                int sum=mul+pos[p2];
+
+                pos[p1]+=sum/10;
+                pos[p2]=sum%10;
+            }
+        }
+        StringBuilder str = new StringBuilder();
+        for(int p:pos){
+            if(!(str.length()==0 && p==0))
+            str.append(p);
+        }
+        return (str.length()==0)?"0":str.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(multiply("99","99"));
+    }
+}
